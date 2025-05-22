9330c25011274845af378ddc5718e8fc37644a68
Lucas Burlingham <lucas@lucasburlingham.me>
2025-05-21 22:26:06 -0400

    Update price script and database

--- scrape.py
+++ scrape.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from urllib.parse import urlparse
 import matplotlib.pyplot as plt
 
-urls = ['https://www.hamradio.com/detail.cfm?pid=H0-016772', 'https://www.radioddity.com/products/xiegu-g90-hf-transceiver', 'https://amazon.com/dp/B08X6Z6KN2']
+urls = ['https://www.hamradio.com/detail.cfm?pid=H0-016772', 'https://www.radioddity.com/products/xiegu-g90-hf-transceiver', 'https://www.amazon.com/dp/B08X6Z6KN2']
 hostname = None
 
 prices = []
@@ -41,14 +41,22 @@ for url in urls:
 			price = 'Not available'
 
 	elif 'amazon' in url:
-		parent = soup.find('span', class_='a-price')
-		# Get the child element money of parent
-		whole = parent.find('span', class_='a-price-whole').text
-		fraction = parent.find('span', class_='a-price-fraction').text
-		price = "$" + whole + fraction
-  
-		if price is None:
-			price = 'Not available'
+		# <span class="a-price aok-align-center reinventPricePriceToPayMargin priceToPay" data-a-size="xl" data-a-color="base">
+		# <span class="a-offscreen"> </span><span aria-hidden="true"><span class="a-price-symbol">$</span>
+		# <span class="a-price-whole">465<span class="a-price-decimal">.
+		# </span></span><span class="a-price-fraction">00</span></span></span>
+
+		# There are multiple elements with class 'a-price'
+		# Find the first occurrence of class 'a-price'
+		parent = soup.find_all('span', class_='a-price')
+		if parent:
+			first_parent = parent[0]
+			whole_elem = first_parent.find('span', class_='a-price-whole')
+			fraction_elem = first_parent.find('span', class_='a-price-fraction')
+			if whole_elem and fraction_elem:
+				whole = whole_elem.text.strip().replace(',', '')
+				fraction = fraction_elem.text.strip()
+				price = "$" + whole + "." + fraction
    
 	elif 'hamradio' in url:
 		parent = soup.find('p', class_='addtoCart')
@@ -74,48 +82,28 @@ print("The average price is $" + average_price)
 lowest_price = min([float(price.replace('$', '')) for price in prices])
 print("The lowest price is $" + "{:.2f}".format(lowest_price) + " at " + urls[prices.index("${:.2f}".format(lowest_price))] + ", which is $" + ("{:.2f}".format(abs(lowest_price - float(average_price)))) + " less than the average price.")
 
-# Generate a graphic with the prices
 
-# Convert prices to float for plotting
-prices_float = [float(price.replace('$', '')) for price in prices]
-
-# Extract hostnames from URLs
-hostnames = [urlparse(url).hostname for url in urls]
-
-plt.bar(hostnames, prices_float)
-plt.ylabel('Price')
-plt.xlabel('Website')
-plt.ylim(min(prices_float) - 10, max(prices_float) + 10)
-print(min(prices_float) - 50, max(prices_float) + 30)
-plt.title('Prices of the Xiegu G90')
-plt.savefig('prices.png')
-plt.show()
-
-# Generate historical data with the db for each website
+# Generate a graphic with the prices over time
+# Get the dates from the database
 conn = sqlite3.connect('G90_prices.db')
 c = conn.cursor()
-c.execute('SELECT * FROM prices')
-data = c.fetchall()
+c.execute('SELECT date, price FROM prices')
+rows = c.fetchall()
+dates = []
+prices = []
+for row in rows:
+	dates.append(row[0])
+	prices.append(float(row[1]))
 conn.close()
 
-
-
-# Group data by hostname
-historical_data = {}
-for row in data:
-	date, price, hostname = row
-	if hostname not in historical_data:
-		historical_data[hostname] = {'dates': [], 'prices': []}
-	historical_data[hostname]['dates'].append(date)
-	historical_data[hostname]['prices'].append(float(price.replace('$', '')))
-
-# Plot historical data for each website
-for hostname, values in historical_data.items():
-	plt.plot(values['dates'], values['prices'], label=hostname)
-
-plt.ylabel('Price')
+# Convert dates to datetime objects
+dates = [datetime.strptime(date, '%Y-%m-%d') for date in dates]
+# Plot the prices
+plt.plot(dates, prices)
 plt.xlabel('Date')
-plt.title('Historical Prices of the Xiegu G90')
-plt.legend()
-plt.savefig('historical_prices.png')
+plt.ylabel('Price')
+plt.title('Prices of Xiegu G90 over time')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.savefig('G90_prices.png')
 plt.show()
